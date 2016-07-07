f65cb2604380d7e25111068bcb3ef328eea1d1f3
Chad Bowen <Chadbowen248@gmail.com>
2016-07-06 22:13:49 -0500

    added plot and option to search for the next item in results

--- scripts.js
+++ scripts.js
@@ -1,47 +1,81 @@
+
+// b214b2f6cd4cb56d9c0a986a2215d33f API KEY!!!!!
+// 'http://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query=fight+club
+// http://api.themoviedb.org/3/search/movie?api_key=b214b2f6cd4cb56d9c0a986a2215d33f&query=batman+returns
+
+
+
 $(document).ready(function() {
   //This is to remove validation message if no poster image is present//
-  $('#term').focus(function() {
-    var full = $('#poster').has('img').length ? true : false;
-    if(full === false) {
-      $('#poster').empty();
-    }
-  });
-
-  // b214b2f6cd4cb56d9c0a986a2215d33f API KEY!!!!!
-// 'http://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query=fight+club
-
-// http://api.themoviedb.org/3/search/movie?api_key=b214b2f6cd4cb56d9c0a986a2215d33f&query=batman+returns
-var film = $('#term').val();
-
-  var getPoster = function() {
-    //Grab movie title and store it in variable//
-    var film = $('#term').val();
-    //check for error if user has entered anything//
-    if(film == '') {
-      $('#poster').html("<h2 class='loading'> Hey silly billy, enter a movie</h2>");
-    } else {
-      //User didn't screw it up, call the API!!!//
-      $('#poster').html("<h2 class='loading'> Your poster is on the way!!</h2>");
-      $.getJSON("http://api.themoviedb.org/3/search/movie?api_key=b214b2f6cd4cb56d9c0a986a2215d33f&query=" + film, function(json){
-          if(json.total_results != 0){
-            $('#poster').html('<img id="thePoster" src=https://image.tmdb.org/t/p/original' + json.results[0]["poster_path"] + ' />');
-            $('#term').val(" ");
-          } else {
-            $('#poster').html('<h2>Nothing found</h2>');
-            $('#term').val(" ");
-          }
+      $('#term').focus(function() {
+        var full = $('#poster').has('img').length ? true : false;
+        if(full === false) {
+          $('#poster').empty();
+        }
       });
 
-    }
-  }
 
 
+
+      var getPoster = function() {
+        //Grab movie title and store it in variable//
+        var film = $('#term').val();
+        var i = 0;
+        //check for error if user has entered anything//
+        if(film == '') {
+          $('#poster').html("<h2 class='loading'>you forgot something</h2>");
+        } else {
+          //User didn't screw it up, call the API!!!//
+          $('#poster').html("<h2 class='loading'> Your poster is on the way!!</h2>");
+          $.getJSON("http://api.themoviedb.org/3/search/movie?api_key=b214b2f6cd4cb56d9c0a986a2215d33f&query=" + film, function(json){
+              if(json.total_results != 0){
+                $('#poster').html('<img id="thePoster" src=https://image.tmdb.org/t/p/original' + json.results[i]["poster_path"] + ' />');
+                $('#plot').html('<h2 class="loading">' + json.results[i]["overview"] + ' </h2>');
+                $('#term').click(function(){
+                    $('#term').val(" ");
+                })
+                $('#similar').html('<h3> Find others like this</h3>');
+              } else {
+                $('#poster').html('<h2>Nothing found</h2>');
+                $('#term').val(" ");
+              }
+          });
+
+        }
+      }
+
+
+
+      var findSimilar = function(){
+        var film = $('#term').val();
+        $.getJSON("http://api.themoviedb.org/3/search/movie?api_key=b214b2f6cd4cb56d9c0a986a2215d33f&query=" + film, function(json){
+                var i = 1;
+                if(json.total_results != 0){
+                  $('#poster').html('<img id="thePoster" src=https://image.tmdb.org/t/p/original' + json.results[i]["poster_path"] + ' />');
+                  $('#plot').html('<h2 class="loading">' + json.results[i]["overview"] + ' </h2>');
+                  $('#term').click(function(){
+                      $('#term').val(" ");
+                  })
+                  $('#similar').html('<h3> Find others like this</h3>');
+                }
+              });
+      };
+
+
+
+
+// click GET POSTER text to run
   $('#search').click(getPoster);
+  // hit ENTER to run
   $('#term').keyup(function(event){
     if(event.keyCode == 13){
       getPoster()
     }
   })
+// click find similar to search for next index
+  $('#similar').click(function(){
+    findSimilar();
+  });
 
 
 });
